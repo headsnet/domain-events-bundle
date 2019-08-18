633848c3407ce3f7c83ceccef6a85bb074343c1b
Ben Roberts <ben@headsnet.com>
2019-08-18 19:56:20 +0200

    Correctly store the aggregate root id in a database column

--- Domain/Model/StoredEvent.php
+++ Domain/Model/StoredEvent.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of the Symfony HeadsnetDomainEventsBundle.
+ *
+ * (c) Headstrong Internet Services Ltd 2019
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Headsnet\DomainEventsBundle\Domain\Model;
@@ -31,7 +40,7 @@ class StoredEvent
 	/**
 	 * @var string
 	 */
-	private $rootId;
+	private $aggregateRoot;
 
 	/**
 	 * @var string
@@ -60,7 +69,7 @@ class StoredEvent
 	{
 		$this->typeName = $typeName;
 		$this->occurredOn = $occurredOn;
-		$this->rootId = $rootId;
+		$this->aggregateRoot = $rootId;
 		$this->eventBody = $eventBody;
 		$this->actorId = $actorId;
 	}
@@ -112,9 +121,9 @@ class StoredEvent
 	/**
 	 * @return string
 	 */
-	public function getAggregateRootId(): string
+	public function getAggregateRoot(): string
 	{
-		return $this->rootId;
+		return $this->aggregateRoot;
 	}
 
 	/**
