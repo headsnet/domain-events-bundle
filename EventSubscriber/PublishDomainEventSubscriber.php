6f9bcb097cae2f6c394fdb010fa2871416a50033
Ben Roberts <ben@headsnet.com>
2019-12-23 17:50:54 +0100

    Use lock component to prevent all race conditions
    Further to the previous commit, this commit replaces the temporary
    sleep() statement with the symfony/lock component.
    
    This should now prevent all race conditions, and also does not extend
    the kernel.terminate duration unnecessarily.

--- EventSubscriber/PublishDomainEventSubscriber.php
+++ EventSubscriber/PublishDomainEventSubscriber.php
@@ -13,74 +13,95 @@ declare(strict_types=1);
 namespace Headsnet\DomainEventsBundle\EventSubscriber;
 
 use Headsnet\DomainEventsBundle\Domain\Model\EventStore;
+use Headsnet\DomainEventsBundle\Domain\Model\StoredEvent;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\Console\Event\ConsoleTerminateEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 final class PublishDomainEventSubscriber implements EventSubscriberInterface
 {
-	/**
-	 * @var MessageBusInterface
-	 */
-	private $eventBus;
+    /**
+     * @var MessageBusInterface
+     */
+    private $eventBus;
 
-	/**
-	 * @var EventStore
-	 */
-	private $eventStore;
+    /**
+     * @var EventStore
+     */
+    private $eventStore;
 
-	/**
-	 * @var SerializerInterface
-	 */
-	private $serializer;
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
 
-	public function __construct(MessageBusInterface $eventBus, EventStore $eventStore, SerializerInterface $serializer)
-	{
-		$this->eventBus = $eventBus;
-		$this->eventStore = $eventStore;
-		$this->serializer = $serializer;
-	}
+    /**
+     * @var LockFactory
+     */
+    private $lockFactory;
 
-	/**
+    public function __construct(
+        MessageBusInterface $eventBus,
+        EventStore $eventStore,
+        SerializerInterface $serializer,
+        LockFactory $lockFactory
+    )
+    {
+        $this->eventBus = $eventBus;
+        $this->eventStore = $eventStore;
+        $this->serializer = $serializer;
+        $this->lockFactory = $lockFactory;
+    }
+
+    /**
      * Support publishing events on TERMINATE event of both HttpKernel and Console
-	 */
-	public static function getSubscribedEvents(): array
-	{
-		return [
-			KernelEvents::TERMINATE => 'publishEventsFromHttp',
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::TERMINATE => 'publishEventsFromHttp',
             ConsoleEvents::TERMINATE => 'publishEventsFromConsole'
-		];
-	}
+        ];
+    }
 
-	public function publishEventsFromHttp(PostResponseEvent $event): void
-	{
-	    // Poor mans fix to avoid race conditions. To be replaced by the symfony/lock component
-        if ($event->getRequest()->getMethod() == 'GET')
-        {
-            sleep(2);
-        }
-
-        $this->publish();
-	}
+    public function publishEventsFromHttp(PostResponseEvent $event): void
+    {
+        $this->publishEvents();
+    }
 
     public function publishEventsFromConsole(ConsoleTerminateEvent $event)
     {
-        $this->publish();
-	}
+        $this->publishEvents();
+    }
 
-    private function publish()
+    private function publishEvents()
     {
         foreach ($this->eventStore->allUnpublished() as $event)
+        {
+            $this->publishEvent($event);
+        }
+    }
+
+    private function publishEvent(StoredEvent $event): void
+    {
+        $lock = $this->lockFactory->createLock(
+            sprintf('domain-event-%s', $event->getEventId()->asString())
+        );
+
+        if ($lock->acquire())
         {
             $this->eventBus->dispatch(
                 $this->serializer->deserialize($event->getEventBody(), $event->getTypeName(), 'json')
             );
 
             $this->eventStore->publish($event);
+
+            $lock->release();
         }
-	}
+    }
 }
