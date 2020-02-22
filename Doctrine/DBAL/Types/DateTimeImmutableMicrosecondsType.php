aa4e9bf5699053b65df633988154b3cc6bac30aa
Ben Roberts <ben@headsnet.com>
2020-02-22 20:24:45 +0100

    Store event timestamps with microseconds
    Add a custom DBAL type to support the DB storing timestamps with
    microseconds.
    
    This allows to properly sort events in the order that they were
    actually received, as many events can otherwise be received in the
    previous granularity of 1 second.

---
+++ Doctrine/DBAL/Types/DateTimeImmutableMicrosecondsType.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the Symfony HeadsnetDomainEventsBundle.
+ *
+ * (c) Headstrong Internet Services Ltd 2020
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Headsnet\DomainEventsBundle\Doctrine\DBAL\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\MySQL57Platform;
+use Doctrine\DBAL\Platforms\PostgreSqlPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\VarDateTimeImmutableType;
+
+class DateTimeImmutableMicrosecondsType extends VarDateTimeImmutableType
+{
+    public function getName()
+    {
+        return 'datetime_immutable_microseconds';
+    }
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        if (isset($fieldDeclaration['version']) && $fieldDeclaration['version'] == true)
+        {
+            return 'TIMESTAMP';
+        }
+
+        return 'DATETIME(6)';
+    }
+
+    /**
+     * @throws ConversionException
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if (\is_object($value) && $value instanceof \DateTimeImmutable &&
+            ($platform instanceof PostgreSqlPlatform || $platform instanceof MySQL57Platform)
+        ) {
+            $dateTimeFormat = $platform->getDateTimeFormatString();
+
+            return $value->format("{$dateTimeFormat}.u");
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+}
