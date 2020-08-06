b875d3c999ba7d655427b164b3e0d21c24cb28e8
Ben Roberts <ben@headsnet.com>
2020-08-06 11:07:05 +0200

    Move source code in to src/ subdir

--- src/DependencyInjection/HeadsnetDomainEventsExtension.php
+++ src/DependencyInjection/HeadsnetDomainEventsExtension.php
@@ -1,8 +1,8 @@
 <?php
-/**
+/*
  * This file is part of the Symfony HeadsnetDomainEventsBundle.
  *
- * (c) Headstrong Internet Services Ltd 2019
+ * (c) Headstrong Internet Services Ltd 2020
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- src/Domain/Model/EventStore.php
+++ src/Domain/Model/EventStore.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * This file is part of the Symfony HeadsnetDomainEventsBundle.
  *
  * (c) Headstrong Internet Services Ltd 2020
