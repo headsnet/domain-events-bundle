3dd5bb084c124568e400345f05b4646a28b1084f
Wolfgang Klinger <wk@plan2.net>
2020-10-26 09:47:00 +0100

    Dispatch an event before appending event to event store

---
+++ src/Doctrine/Event/PreAppendEvent.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Headsnet\DomainEventsBundle\Doctrine\Event;
+
+use Headsnet\DomainEventsBundle\Domain\Model\DomainEvent;
+use Symfony\Contracts\EventDispatcher\Event;
+
+/**
+ * Class PreAppendEvent.
+ *
+ * @author Wolfgang Klinger <wolfgang@wazum.com>
+ */
+class PreAppendEvent extends Event
+{
+    /**
+     * @var DomainEvent
+     */
+    protected $domainEvent;
+
+    public function __construct(DomainEvent $domainEvent)
+    {
+        $this->domainEvent = $domainEvent;
+    }
+
+    public function getDomainEvent(): DomainEvent
+    {
+        return $this->domainEvent;
+    }
+}
